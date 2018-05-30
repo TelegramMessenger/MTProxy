fcb804cbb0e488ed9f9c04de7b5a0fefbe4d2def
Vitaly Lipatov <lav@etersoft.ru>
2018-05-31 00:50:46 +0300

    fix build with openssh 1.0.x

--- common/sha1.h
+++ common/sha1.h
@@ -18,6 +18,12 @@
               2016 Nikolai Durov
 */
 #include <openssl/evp.h>
+#include <openssl/opensslv.h>
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000
+#define EVP_MD_CTX_new EVP_MD_CTX_create
+#define EVP_MD_CTX_free EVP_MD_CTX_destroy
+#endif
 
 typedef EVP_MD_CTX sha1_context;
 
